fb69a17dcf10e6f4fa29531fa1dd39d18c224f44
fahmida0404 <fahmidaakter0404@gmail.com>
2025-10-25 20:29:21 +0600

    Update water reminder

--- water_reminder/reminder.py
+++ water_reminder/reminder.py
@@ -5,5 +5,5 @@ while True:
     notifier = ToastNotifier()
     notifier.show_toast("Drink Water!","It's been 2 hours. Time to drink a glass of water.",
                         duration = 5,
-                        icon_path="water_reminder\water_drop.ico")
+                        icon_path="python-problems\water_reminder\water_drop.ico")
     time.sleep(30) #adjust according to preference 
